d9c146fb35e9b7e857dd59c36971e385f41fae01
Taine Rasmussen <taine.rasmussen@gmail.com>
2024-12-18 23:53:48 +0000

    clear junk & create password input

--- app/(auth)/SignUpFlow.tsx
+++ app/(auth)/SignUpFlow.tsx
@@ -1,19 +1,33 @@
-import React from "react";
-import { YStack, H1, SizableText } from "tamagui";
+import React, { useState } from "react";
+import { YStack, H1, SizableText, Input } from "tamagui";
+import InputWithIcon from "@/components/PasswordInput";
 
 interface ISignUpFlow {
   handleViewToggle: () => void;
 }
 
 const SignUpFlow = (props: ISignUpFlow) => {
+  const [value, setValue] = useState<string>("");
   return (
-    <YStack alignSelf="center">
-      <H1>Create an account</H1>
-      <SizableText theme="alt1" size="$3">
-        Already have an account?{" "}
-        <span onClick={props.handleViewToggle}>Log in</span>
-      </SizableText>
-    </YStack>
+    <>
+      <YStack alignSelf="center">
+        <H1>Create an account</H1>
+        <SizableText theme="alt1" size="$3">
+          Already have an account?{" "}
+          <span
+            onClick={props.handleViewToggle}
+            style={{ textDecorationLine: "underline", cursor: "pointer" }}
+          >
+            Log in
+          </span>
+        </SizableText>
+      </YStack>
+      <YStack alignSelf="center" gap={8}>
+        <Input flex={1} size="$5" placeholder="Name..." borderWidth={3} />
+        <Input flex={1} size="$5" placeholder="Email..." borderWidth={3} />
+        <InputWithIcon value={value} setValue={setValue} />
+      </YStack>
+    </>
   );
 };
 
---
+++ components/PasswordInput.tsx
@@ -0,0 +1,52 @@
+import { Input, XStack, YStack } from "tamagui";
+import { Eye, EyeOff } from "@tamagui/lucide-icons";
+import React, { useState } from "react";
+
+interface IInputWithIcon {
+  value: string;
+  setValue: (value: string) => void;
+}
+
+const InputWithIcon = (props: IInputWithIcon) => {
+  const [isSecureTextEntry, setIsSecureTextEntry] = useState<boolean>(true);
+
+  const toggleSecureTextEntry = () =>
+    setIsSecureTextEntry((prevState) => !prevState);
+
+  return (
+    <YStack padding="$4">
+      <XStack
+        borderWidth={1}
+        borderRadius="$4"
+        alignItems="center"
+        borderColor="$borderColor"
+        backgroundColor="$background"
+      >
+        <Input
+          flex={1}
+          size={"$5"}
+          borderWidth={3}
+          paddingRight={40}
+          value={props.value}
+          placeholder="Password..."
+          onChangeText={props.setValue}
+          secureTextEntry={isSecureTextEntry}
+        />
+        <YStack
+          right={10}
+          height="100%"
+          position="absolute"
+          justifyContent="center"
+        >
+          {isSecureTextEntry ? (
+            <Eye size={20} color="$color" onPress={toggleSecureTextEntry} />
+          ) : (
+            <EyeOff size={20} color="$color" onPress={toggleSecureTextEntry} />
+          )}
+        </YStack>
+      </XStack>
+    </YStack>
+  );
+};
+
+export default InputWithIcon;
