4d6272f95dcdf048bc5bb082173bc395957747af
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-01-04 11:53:14 +0000

    keyboardavoidingView

--- app/(auth)/SignUpFlow/Form.tsx
+++ app/(auth)/SignUpFlow/Form.tsx
@@ -1,72 +1,80 @@
 import PasswordInput from "@/components/PasswordInput";
 import { YStack, XStack, Input, Separator, Button, Text } from "tamagui";
-import { Dimensions, TouchableWithoutFeedback, Keyboard } from "react-native";
-import { useState } from "react";
+import {
+  Dimensions,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+} from "react-native";
+import { useState, useMemo } from "react";
 
 const Form = () => {
   const [value, setValue] = useState<string>("");
   const screenWidth = Dimensions.get("window").width;
-  const inputWidth = screenWidth * 0.9 + 16;
+  const inputWidth = useMemo(() => screenWidth * 0.9 + 16, [screenWidth]);
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <YStack gap={16} alignItems="center">
-        <XStack gap={16}>
-          <Input
-            size="$5"
-            placeholder="First Name..."
-            borderWidth={3}
-            width="45%"
-          />
-          <Input
-            size="$5"
-            placeholder="Last Name..."
-            borderWidth={3}
-            width="45%"
-          />
-        </XStack>
-        <Input
-          size="$5"
-          placeholder="Email..."
-          borderWidth={3}
-          width={inputWidth}
-        />
-        <PasswordInput value={value} setValue={setValue} width={inputWidth} />
-        <YStack gap={16} marginBlock={36}>
-          <Button
-            size="$5"
-            theme="active"
-            justifyContent="center"
-            width={inputWidth}
-          >
-            Create account
-          </Button>
-          <XStack width={inputWidth}>
-            <Separator alignSelf="center" />
-            <Text>Or register with</Text>
-            <Separator alignSelf="center" />
-          </XStack>
+    <KeyboardAvoidingView>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <YStack gap={16} alignItems="center">
           <XStack gap={16}>
-            <Button
+            <Input
               size="$5"
-              variant="outlined"
-              justifyContent="center"
-              width={inputWidth / 2 - 16}
-            >
-              Google
-            </Button>
-            <Button
+              placeholder="First Name..."
+              borderWidth={3}
+              width="45%"
+            />
+            <Input
               size="$5"
-              variant="outlined"
-              justifyContent="center"
-              width={inputWidth / 2 - 16}
-            >
-              Apple
-            </Button>
+              placeholder="Last Name..."
+              borderWidth={3}
+              width="45%"
+            />
           </XStack>
+          <Input
+            size="$5"
+            placeholder="Email..."
+            borderWidth={3}
+            width={inputWidth}
+          />
+          <PasswordInput value={value} setValue={setValue} width={inputWidth} />
+          <YStack gap={16} marginBlock={36}>
+            <Button
+              size="$5"
+              theme="active"
+              justifyContent="center"
+              width={inputWidth}
+              disabled={true}
+            >
+              Create account
+            </Button>
+            <XStack width={inputWidth}>
+              <Separator alignSelf="center" />
+              <Text>Or register with</Text>
+              <Separator alignSelf="center" />
+            </XStack>
+            <XStack gap={16}>
+              <Button
+                size="$5"
+                variant="outlined"
+                justifyContent="center"
+                width={inputWidth / 2 - 16}
+              >
+                Google
+              </Button>
+              <Button
+                size="$5"
+                variant="outlined"
+                justifyContent="center"
+                width={inputWidth / 2 - 16}
+              >
+                Apple
+              </Button>
+            </XStack>
+          </YStack>
         </YStack>
-      </YStack>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
