6fdbb81626c29f8c805e8223896c0fc4addecf24
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-01-05 20:32:00 +0000

    remove toast

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,7 +1,5 @@
 import { InputErrorKeys } from "@/app/types";
 import { Input } from "tamagui";
-import { useEffect, useState } from "react";
-import useToast from "@/hooks/useToast";
 
 interface IFormInput extends React.ComponentProps<typeof Input> {
   value: string;
@@ -12,32 +10,18 @@ interface IFormInput extends React.ComponentProps<typeof Input> {
 }
 
 const FormInput = (props: IFormInput) => {
-  const { showToast } = useToast();
-  const toastMessage = "Invalid text";
-
-  // Use state to trigger the toast
-  const [isTriggered, setIsTriggered] = useState(false);
-
-  // Update state when input changes
-  const handleInputChange = (e: any) => {
-    props.onChange(e);
-    setIsTriggered((prev) => !prev); // Toggle state to trigger `useEffect`
-  };
-
-  useEffect(() => {
-    showToast(toastMessage, "error");
-  }, [isTriggered]); // Effect runs when `isTriggered` changes
-
   return (
-    <Input
-      size="$5"
-      {...props}
-      borderWidth={3}
-      value={props.value}
-      onChangeText={handleInputChange}
-      placeholder={props.placeholder}
-      width={props.width ? props.width : "45%"}
-    />
+    <>
+      <Input
+        size="$5"
+        {...props}
+        borderWidth={3}
+        value={props.value}
+        onChangeText={props.onChange}
+        placeholder={props.placeholder}
+        width={props.width ? props.width : "45%"}
+      />
+    </>
   );
 };
 
