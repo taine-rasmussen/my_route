ce7ca9e9e797b140811b43ff0e9f522f842f8531
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-01-18 10:24:44 +0000

    settings dialog open close'

---
+++ app/(main)/dashboard/userWidget/SettingsDialog.tsx
@@ -0,0 +1,44 @@
+import { Sheet, Text, View } from "tamagui";
+import { ChevronDown } from "@tamagui/lucide-icons";
+
+interface ISettingsDialog {
+  openDialog: boolean;
+  toggleDialog: () => void;
+}
+
+const SettingsDialog = (props: ISettingsDialog) => {
+  return (
+    <Sheet
+      modal={true}
+      dismissOnSnapToBottom
+      open={props.openDialog}
+      snapPoints={[85, 50, 25]}
+      onOpenChange={props.toggleDialog}
+      forceRemoveScrollEnabled={props.openDialog}
+    >
+      <Sheet.Overlay
+        animation="medium"
+        exitStyle={{ opacity: 0 }}
+        enterStyle={{ opacity: 0 }}
+      />
+      <Sheet.Frame
+        flex={1}
+        gap="$5"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <View position="absolute" top={10} right={10}>
+          <ChevronDown size="$3" onPress={props.toggleDialog} />
+        </View>
+        <Text>SETTINGS</Text>
+      </Sheet.Frame>
+    </Sheet>
+  );
+};
+
+export default SettingsDialog;
+
+// TODO:
+// refresh token not refreshing after 15 mins?
+// switch to main app on 200 login
+// linting && TS
--- app/(main)/dashboard/userWidget/UserWidget.tsx
+++ app/(main)/dashboard/userWidget/UserWidget.tsx
@@ -6,16 +6,19 @@ import {
   YStack,
   Separator,
   View,
+  Dialog,
 } from "tamagui";
 import { MapPin, Settings, Home } from "@tamagui/lucide-icons";
 import { useUser } from "@/app/UserContext";
 import { useEffect, useState } from "react";
 import { IUser } from "@/app/types";
+import SettingsDialog from "./SettingsDialog";
 
 const UserWidget = () => {
   const { user } = useUser();
   const [state, setState] = useState<IUser>({} as IUser);
   const [loading, setLoading] = useState<boolean>(false);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
 
   useEffect(() => {
     if (!user?.id) return;
@@ -47,35 +50,50 @@ const UserWidget = () => {
     getUserData();
   }, [user]);
 
+  const handleDialogToggle = () => {
+    setOpenDialog((prevState) => !prevState);
+  };
+
   const username = `${state.first_name} ${state.last_name}`;
 
+  console.log(openDialog);
+
   return (
-    <Card elevate size="$10" bordered>
-      <XStack padding={16} gap={24}>
-        <Avatar circular size="$8">
-          <Avatar.Image accessibilityLabel="profile_picture" src="" />
-          <Avatar.Fallback backgroundColor="$blue10" />
-        </Avatar>
-        <YStack gap={4}>
-          <SizableText size="$6">{username}</SizableText>
-          <Separator />
-          <XStack gap={8}>
-            <XStack gap={4}>
-              <MapPin size="$1" />
-              <SizableText size="$4">{state.location}</SizableText>
+    <>
+      <SettingsDialog
+        openDialog={openDialog}
+        toggleDialog={handleDialogToggle}
+      />
+      <Card elevate size="$10" bordered>
+        <XStack padding={16} gap={24}>
+          <Avatar circular size="$8">
+            <Avatar.Image
+              accessibilityLabel="profile_picture"
+              src="/Users/tainerasmussen/Desktop/my_route/assets/images/favicon.png"
+            />
+            <Avatar.Fallback backgroundColor="$blue10" />
+          </Avatar>
+          <YStack gap={4}>
+            <SizableText size="$6">{username}</SizableText>
+            <Separator />
+            <XStack gap={8}>
+              <XStack gap={4}>
+                <MapPin size="$1" />
+                <SizableText size="$4">{state.location}</SizableText>
+              </XStack>
+              <Separator vertical />
+              <XStack gap={4}>
+                <Home size="$1" />
+                <SizableText size="$4">{state.home_gym}</SizableText>
+              </XStack>
             </XStack>
-            <Separator vertical />
-            <XStack gap={4}>
-              <Home size="$1" />
-              <SizableText size="$4">{state.home_gym}</SizableText>
-            </XStack>
-          </XStack>
-        </YStack>
-        <View position="absolute" top={16} right={16}>
-          <Settings size="$2" />
-        </View>
-      </XStack>
-    </Card>
+          </YStack>
+          <View position="absolute" top={16} right={16}>
+            <Settings size="$2" onPress={handleDialogToggle} />
+          </View>
+        </XStack>
+      </Card>
+    </>
   );
 };
 
