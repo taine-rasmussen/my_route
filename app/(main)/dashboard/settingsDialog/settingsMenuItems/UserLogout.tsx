d2dc93db5d4f17928e42f8c71c166bc1902608e2
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-01-21 11:53:22 +0000

    use alert dialog for log out

--- app/(main)/dashboard/settingsDialog/settingsMenuItems/UserLogout.tsx
+++ app/(main)/dashboard/settingsDialog/settingsMenuItems/UserLogout.tsx
@@ -1,20 +1,62 @@
 import { LogOut } from '@tamagui/lucide-icons';
-import { Button } from 'tamagui';
+import { Button, AlertDialog, XStack } from 'tamagui';
 
 const UserLogout = () => {
   // Plug into backend logout
   // remove tokens
   // navigate to login flow
+
+  const handleSignOut = () => {
+    console.log('SIGNED OUT');
+  };
+
   return (
-    <Button
-      size="$6"
-      color="salmon"
-      borderWidth={1}
-      borderColor="grey"
-      iconAfter={LogOut}
-    >
-      Sign out
-    </Button>
+    <AlertDialog native>
+      <AlertDialog.Trigger asChild>
+        <Button
+          size="$6"
+          color="salmon"
+          borderWidth={1}
+          borderColor="grey"
+          iconAfter={LogOut}
+        >
+          Sign out
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Portal>
+        <AlertDialog.Content
+          bordered
+          elevate
+          key="content"
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+          x={0}
+          scale={1}
+          opacity={1}
+          y={0}
+        >
+          <AlertDialog.Title>Are you sure?</AlertDialog.Title>
+          <XStack gap="$3" justifyContent="flex-end">
+            <AlertDialog.Cancel asChild>
+              <Button>No</Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action asChild>
+              <Button theme="active" onPress={handleSignOut}>
+                Yes
+              </Button>
+            </AlertDialog.Action>
+          </XStack>
+        </AlertDialog.Content>
+      </AlertDialog.Portal>
+    </AlertDialog>
   );
 };
 
