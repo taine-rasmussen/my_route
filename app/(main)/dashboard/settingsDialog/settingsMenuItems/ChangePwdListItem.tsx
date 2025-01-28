f57f3584a225cb625f0e717c60958597c9a8cdbc
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-01-28 12:36:42 +0000

    add menu item for changing pwd

---
+++ app/(main)/dashboard/settingsDialog/settingsMenuItems/ChangePwdListItem.tsx
@@ -0,0 +1,23 @@
+import { ListItem, SizableText, XStack, Button } from 'tamagui';
+import { Lock } from '@tamagui/lucide-icons';
+
+const ChangePwdListListItem = () => {
+  return (
+    <ListItem>
+      <XStack
+        flex={1}
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <XStack gap={8} alignItems="center">
+          <Lock size="$1" />
+          <SizableText size="$6">Password</SizableText>
+        </XStack>
+        <Button>Edit</Button>
+      </XStack>
+    </ListItem>
+  );
+};
+
+export default ChangePwdListListItem;
