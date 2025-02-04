b19f2bd2f9cc7823138797c2fdc11a3808953be9
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-02-04 12:09:02 +0000

    set up form for pwd change

--- app/(main)/dashboard/settingsDialog/settingsMenuItems/ChangePwdListItem.tsx
+++ app/(main)/dashboard/settingsDialog/settingsMenuItems/ChangePwdListItem.tsx
@@ -1,9 +1,33 @@
 import { useState } from 'react';
 import { ListItem, SizableText, XStack, Button, Sheet, YStack } from 'tamagui';
 import { Lock } from '@tamagui/lucide-icons';
+import PasswordInput from '@/components/PasswordInput';
+import { InputErrorKeys } from '@/app/types';
+
+const initState = {
+  currentPassword: false,
+  newPassword: false,
+  confirmNewPassword: false,
+};
 
 const ChangePwdListListItem = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [currentPassword, setCurrentPassword] = useState<string>('');
+  const [newPassword, setNewPassword] = useState<string>('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState<string>('');
+  const [inputErrors, setInputErrors] = useState(initState);
+
+  const setError: (field: InputErrorKeys, value: boolean) => void = (
+    field,
+    value,
+  ) => {
+    setInputErrors((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
+  console.log(isOpen);
 
   return (
     <>
@@ -27,7 +51,30 @@ const ChangePwdListListItem = () => {
           <Sheet.ScrollView>
             <YStack padding={16} gap={12}>
               <SizableText size="$6">Reset Password</SizableText>
-
+              <YStack>
+                <PasswordInput
+                  setError={setError}
+                  value={currentPassword}
+                  onChange={setCurrentPassword}
+                  placeholder="Current password..."
+                  error={inputErrors.currentPassword}
+                />
+                <PasswordInput
+                  value={newPassword}
+                  setError={setError}
+                  onChange={setNewPassword}
+                  placeholder="New Password..."
+                  error={inputErrors.newPassword}
+                />
+                <PasswordInput
+                  setError={setError}
+                  value={confirmNewPassword}
+                  onChange={setConfirmNewPassword}
+                  placeholder="Confirm New Password..."
+                  error={inputErrors.confirmNewPassword}
+                />
+              </YStack>
+              <Button>Save</Button>
               <Button onPress={() => setIsOpen(false)}>Close</Button>
             </YStack>
           </Sheet.ScrollView>
--- components/PasswordInput.tsx
+++ components/PasswordInput.tsx
@@ -1,7 +1,7 @@
-import { Input, XStack, YStack, Text } from "tamagui";
-import { AlertCircle, Eye, EyeOff } from "@tamagui/lucide-icons";
-import React, { useState } from "react";
-import { InputErrorKeys } from "@/app/types";
+import { Input, XStack, YStack, Text } from 'tamagui';
+import { AlertCircle, Eye, EyeOff } from '@tamagui/lucide-icons';
+import React, { useState } from 'react';
+import { InputErrorKeys } from '@/app/types';
 
 interface IPasswordInput {
   onChange: (value: string) => void;
@@ -9,25 +9,26 @@ interface IPasswordInput {
   width?: number | string;
   error: boolean;
   setError: (field: InputErrorKeys, value: boolean) => void;
+  placeholder?: string;
 }
 
 const PasswordInput = (props: IPasswordInput) => {
   const [isSecureTextEntry, setIsSecureTextEntry] = useState<boolean>(true);
-  const errorMessage = "Password must be at least 7 characters long";
+  const errorMessage = 'Password must be at least 7 characters long';
 
   const toggleSecureTextEntry = () =>
     setIsSecureTextEntry((prevState) => !prevState);
 
   const handleBlur = () => {
     if (props.value.length < 7) {
-      return props.setError("password", true);
+      return props.setError('password', true);
     } else {
-      return props.setError("password", false);
+      return props.setError('password', false);
     }
   };
 
   return (
-    <YStack width={props.width || "auto"} space={4}>
+    <YStack width={props.width || 'auto'} space={4}>
       <XStack
         borderRadius="$4"
         alignItems="center"
@@ -36,11 +37,11 @@ const PasswordInput = (props: IPasswordInput) => {
       >
         <Input
           flex={1}
-          size={"$5"}
+          size={'$5'}
           borderWidth={3}
           onBlur={handleBlur}
           value={props.value}
-          placeholder="Password..."
+          placeholder={props.placeholder ? props.placeholder : 'Password...'}
           onChangeText={props.onChange}
           secureTextEntry={isSecureTextEntry}
         />
@@ -60,7 +61,7 @@ const PasswordInput = (props: IPasswordInput) => {
       <YStack height={24}>
         {props.error && (
           <XStack paddingInlineStart={8} alignItems="center">
-            <AlertCircle size={"$1"} color="red" />
+            <AlertCircle size={'$1'} color="red" />
             <Text paddingLeft={8} color="red">
               {errorMessage}
             </Text>
