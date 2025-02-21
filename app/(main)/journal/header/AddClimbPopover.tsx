4533c692212a47b1e862e53f218025f9d8c659c3
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-02-21 11:46:08 +0000

    refactor to use new drop down

--- app/(main)/journal/header/AddClimbPopover.tsx
+++ app/(main)/journal/header/AddClimbPopover.tsx
@@ -1,4 +1,4 @@
-import { CirclePlus, ChevronDown, Check } from '@tamagui/lucide-icons';
+import { CirclePlus } from '@tamagui/lucide-icons';
 import {
   Popover,
   SizableText,
@@ -7,21 +7,21 @@ import {
   Label,
   Button,
   Input,
-  Select,
-  Adapt,
-  Sheet,
 } from 'tamagui';
 import { useState } from 'react';
+import { Dropdown } from 'react-native-element-dropdown';
 import { VGrade } from '@/app/types';
+import { StyleSheet } from 'react-native';
 
-const grades: VGrade[] = Array.from(
-  { length: 18 },
-  (_, i) => `V${i}` as VGrade,
-);
+const grades = Array.from({ length: 18 }, (_, i) => ({
+  label: `V${i}`,
+  value: `V${i}`,
+}));
 
 const AddClimbPopover = () => {
   const [attempts, setAttempts] = useState<number>(0);
   const [grade, setGrade] = useState<VGrade>('V0');
+  const [isFocus, setIsFocus] = useState(false);
 
   return (
     <Popover size="$5" allowFlip stayInFrame offset={10} placement="bottom">
@@ -37,14 +37,7 @@ const AddClimbPopover = () => {
         enterStyle={{ y: -10, opacity: 0 }}
         exitStyle={{ y: -10, opacity: 0 }}
         elevate
-        animation={[
-          'bouncy',
-          {
-            opacity: {
-              overshootClamping: true,
-            },
-          },
-        ]}
+        animation={['bouncy', { opacity: { overshootClamping: true } }]}
       >
         <Popover.Arrow borderWidth={1} borderColor="$borderColor" />
         <YStack gap="$3" width={'100%'}>
@@ -52,37 +45,22 @@ const AddClimbPopover = () => {
 
           <XStack gap="$3" alignItems="center">
             <Label size="$3">Grade</Label>
-            <Select
+            <Dropdown
+              style={[styles.dropdown, isFocus && { borderColor: 'white' }]}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              data={grades}
+              labelField="label"
+              valueField="value"
+              placeholder="Select a grade"
               value={grade}
-              onValueChange={(value) => setGrade(value as VGrade)}
-            >
-              <Select.Trigger width={120} iconAfter={ChevronDown}>
-                <Select.Value placeholder="Select a grade" />
-              </Select.Trigger>
-
-              <Adapt when="sm" platform="touch">
-                <Sheet native modal dismissOnSnapToBottom animation="medium">
-                  <Sheet.Frame>
-                    <Sheet.ScrollView>
-                      <Adapt.Contents />
-                    </Sheet.ScrollView>
-                  </Sheet.Frame>
-                </Sheet>
-              </Adapt>
-
-              <Select.Content zIndex={200000}>
-                <Select.Viewport>
-                  {grades.map((g, i) => (
-                    <Select.Item key={g} index={i} value={g}>
-                      <Select.ItemText>{g}</Select.ItemText>
-                      <Select.ItemIndicator marginLeft="auto">
-                        <Check size={16} />
-                      </Select.ItemIndicator>
-                    </Select.Item>
-                  ))}
-                </Select.Viewport>
-              </Select.Content>
-            </Select>
+              onFocus={() => setIsFocus(true)}
+              onBlur={() => setIsFocus(false)}
+              onChange={(item) => {
+                setGrade(item.value as VGrade);
+                setIsFocus(false);
+              }}
+            />
           </XStack>
 
           <XStack gap="$3">
@@ -103,5 +81,24 @@ const AddClimbPopover = () => {
     </Popover>
   );
 };
+
+const styles = StyleSheet.create({
+  dropdown: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    flex: 1,
+  },
+  placeholderStyle: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+    color: 'black',
+  },
+});
 
 export default AddClimbPopover;
