c02a350ab61ddc6d8de1101529bcafe09b7611c4
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-04-03 16:35:27 +0100

    add charts

--- app/(main)/dashboard/widgets/journalWidget/AverageClimbWidget.tsx
+++ app/(main)/dashboard/widgets/journalWidget/AverageClimbWidget.tsx
@@ -1,23 +1,21 @@
 import { getFromSecureStore } from '@/app/utils';
 import { useState, useEffect } from 'react';
 import { Card, SizableText, XStack } from 'tamagui';
+import moment from 'moment';
+import { VGrade } from '@/app/types';
 
 const AverageClimbWidget = () => {
-  const [averageClimb, setAverageClimb] = useState(null);
-
-  console.log('AVG GRADE', averageClimb);
+  const [averageClimb, setAverageClimb] = useState<VGrade | null>(null);
 
   useEffect(() => {
     async function fetchAverageGrade() {
       const filters = {
-        start_date: '2023-04-01T00:00:00',
-        end_date: '2025-03-30T23:59:59',
+        start_date: moment().subtract(1, 'months').format('YYYY-MM-DD'),
+        end_date: moment().format('YYYY-MM-DD'),
       };
 
       try {
-        // Retrieve the token from secure storage
         const accessToken = await getFromSecureStore('access_token');
-        // Call the average_grade endpoint with an empty JSON body for all-time average
         const response = await fetch(
           `${process.env.EXPO_PUBLIC_BASE_URL}average_grade/`,
           {
@@ -31,7 +29,7 @@ const AverageClimbWidget = () => {
         );
         const data = await response.json();
         if (response.ok) {
-          setAverageClimb(data.average_grade);
+          setAverageClimb(`V${data.average_grade}` as VGrade);
         } else {
           console.error('Error fetching average grade:', data);
         }
@@ -42,11 +40,14 @@ const AverageClimbWidget = () => {
     fetchAverageGrade();
   }, []);
 
+  // TODO: press on card has pop up to show toggle hard coded dates for average range / custom - reuse date picker.
+
   return (
     <Card padding={16} elevate size="$5" bordered width="45%" margin="auto">
       <XStack gap={12} alignItems="center">
         <SizableText>
-          Average grade: {averageClimb !== null ? averageClimb : 'Loading...'}
+          Last months average:
+          {averageClimb !== null ? averageClimb : 'Loading...'}
         </SizableText>
       </XStack>
     </Card>
