4b5ec4e6e550b073fe1e30678bcef164a8bbd63d
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-04-26 13:52:23 +0100

    projects mvp!

--- app/(main)/projects/index.tsx
+++ app/(main)/projects/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SafeAreaWrapper from '@/components/SafeAreaWrapper';
 import Header from './Header';
 import { ProjectProvider } from '@/app/contexts/ProjectsContext';
+import ProjectsList from './ProjectsList';
 
 const Projects = () => {
   return (
     <ProjectProvider>
       <SafeAreaWrapper>
         <Header />
+        <ProjectsList />
       </SafeAreaWrapper>
     </ProjectProvider>
   );
